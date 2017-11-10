09b87120592e37918701c50cb7490d2f73281110
Seung Jin An <sa190@duke.edu>
2017-11-09 20:36:07 -0500

    Minor class path change

--- src/spring/configuration/HibernateConfiguration.java
+++ src/spring/configuration/HibernateConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan({ "configuration" })
+@ComponentScan({ "spring.configuration" })
 @PropertySource(value = { "classpath:application.properties" })
 public class HibernateConfiguration {
 
