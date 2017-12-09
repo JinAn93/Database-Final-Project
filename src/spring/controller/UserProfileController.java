cec07f888a17f77055af27c31a87fc266738756d
Seung Jin An <sa190@duke.edu>
2017-12-08 20:57:42 -0500

    Changed DB Credentials

--- src/spring/controller/UserProfileController.java
+++ src/spring/controller/UserProfileController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import spring.configuration.SSHConnection;
 import spring.model.User;
 import spring.service.IPostService;
 import spring.service.IUserService;
@@ -29,14 +28,6 @@ public class UserProfileController {
 //		if (model.containsAttribute("user_id")) {
 //			return "redirect:/dashboard";
 //		}
-		try {
-			SSHConnection ssh = new SSHConnection();
-			System.out.println("Successfully connected to SSH");
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			System.out.println("No Connection for SSH");
-			e.printStackTrace();
-		}
 		User user = userService.findById("root");	//some username		
 		System.out.println("Name: " + user.getFirst_name());
 		model.addAttribute("test", user);		
